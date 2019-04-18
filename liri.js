a057dac8fe0b8af9fe01bbb58dd6d177c899fee5
ArthurM0rgan <46456968+TonyPrpich@users.noreply.github.com>
2019-04-18 13:03:19 -0600

    updated json

--- liri.js
+++ liri.js
@@ -13,120 +13,120 @@ var textFile = "log.txt";
 commandSwitch(cmd, arg);
 
 function commandSwitch(cmd, arg) {
-  switch (cmd) {
-    case 'concert-this':
-      concertThis(arg);
-      break;
-    case 'spotify-this-song':
-      spotifyThis(arg);
-      break;
-    case 'movie-this':
-      movieThis(arg);
-      break;
-    case 'do-what-it-says':
-      doWhatItSays();
-      break;
-    default:
-      console.log("Please use a valid command.")
-      return;
-  }
+    switch (cmd) {
+        case 'concert-this':
+            concertThis(arg);
+            break;
+        case 'spotify-this-song':
+            spotifyThis(arg);
+            break;
+        case 'movie-this':
+            movieThis(arg);
+            break;
+        case 'do-what-it-says':
+            doWhatItSays();
+            break;
+        default:
+            console.log("Please use a valid command.")
+            return;
+    }
 }
 
 function concertThis(arg) {
-  var artist = arg;
-  var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp&tracker_count=10";
-  request(queryUrl, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      body = JSON.parse(body);
-      for (var event in body) {
-        display("Venue: ", body[event].venue.name);
-        display("Location: ", body[event].venue.city + ", " + body[event].venue.region + ", " + body[event].venue.country);
-        var m = moment(body[event].datetime).format('MM/DD/YYYY, h:mm a').split(", ");
-        display("Date: ", m[0]);
-        display("Time: ", m[1]);
-        contentAdded();
-      }
-    }
-  });
+    var artist = arg;
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp&tracker_count=10";
+    request(queryUrl, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            body = JSON.parse(body);
+            for (var event in body) {
+                display("Venue: ", body[event].venue.name);
+                display("Location: ", body[event].venue.city + ", " + body[event].venue.region + ", " + body[event].venue.country);
+                var m = moment(body[event].datetime).format('MM/DD/YYYY, h:mm a').split(", ");
+                display("Date: ", m[0]);
+                display("Time: ", m[1]);
+                contentAdded();
+            }
+        }
+    });
 }
 //spotify-this function
 function spotifyThis(arg) {
-  var song = arg;
-  if (!song) {
-    song = "The+Sign";
-    console.log(song);
-  }
-  spotify.search({
-    type: 'track',
-    query: song
-  }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
+    var song = arg;
+    if (!song) {
+        song = "The+Sign";
+        console.log(song);
     }
-    data = data.tracks.items[0];
-    
-    display("Artist(s) Name: ", data.artists[0].name);
-    display("Track Name: ", data.name);
-    display("Preview URL: ", data.preview_url);
-    display("Album: ", data.album.name);
-    contentAdded();
-  });
+    spotify.search({
+        type: 'track',
+        query: song
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        data = data.tracks.items[0];
+
+        display("Artist(s) Name: ", data.artists[0].name);
+        display("Track Name: ", data.name);
+        display("Preview URL: ", data.preview_url);
+        display("Album: ", data.album.name);
+        contentAdded();
+    });
 }
 
 function movieThis(arg) {
-  var movieName = arg;
-  if (!movieName) {
-    movieName = "Mr.+Nobody"
-  };
-  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-  request(queryUrl, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      body = JSON.parse(body);
-      display("Title: ", body.Title);
-      display("Year: ", body.Year);
-      display("IMDB Rating: ", body.imdbRating);
-      if (body.Ratings[2]) {
-        display("Rotten Tomatoes Score: ", body.Ratings[2].Value);
-      }
-      display("Country: ", body.Country);
-      display("Language: ", body.Language);
-      display("Plot: ", body.Plot);
-      display("Actors: ", body.Actors);
-      contentAdded();
-    }
-  });
+    var movieName = arg;
+    if (!movieName) {
+        movieName = "Mr.+Nobody"
+    };
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    request(queryUrl, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            body = JSON.parse(body);
+            display("Title: ", body.Title);
+            display("Year: ", body.Year);
+            display("IMDB Rating: ", body.imdbRating);
+            if (body.Ratings[2]) {
+                display("Rotten Tomatoes Score: ", body.Ratings[2].Value);
+            }
+            display("Country: ", body.Country);
+            display("Language: ", body.Language);
+            display("Plot: ", body.Plot);
+            display("Actors: ", body.Actors);
+            contentAdded();
+        }
+    });
 }
 
 function doWhatItSays() {
-  fs.readFile("random.txt", "utf8", function(error, data) {
-    if (error) {
-      return console.log(error);
-    }
-    var dataArr = data.replace(/(\r\n|\n|\r)/gm, "").split(",");
-    for (var i = 0; i < dataArr.length; i += 2) {
-      var cmd = dataArr[i];
-      var arg = dataArr[i + 1].replace(/['"]+/g, '').split(' ').join("+");
-      commandSwitch(cmd, arg);
-    }
-  });
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        var dataArr = data.replace(/(\r\n|\n|\r)/gm, "").split(",");
+        for (var i = 0; i < dataArr.length; i += 2) {
+            var cmd = dataArr[i];
+            var arg = dataArr[i + 1].replace(/['"]+/g, '').split(' ').join("+");
+            commandSwitch(cmd, arg);
+        }
+    });
 }
 
 function display(description, data) {
-  console.log(description + data);
-  appendFile(description + data + "\n");
+    console.log(description + data);
+    appendFile(description + data + "\n");
 }
 
 function contentAdded() {
-  console.log("");
-  console.log("Content Added!");
-  console.log("-----------------------------------\n");
-  appendFile("-----------------------------------\n");
+    console.log("");
+    console.log("Content Added!");
+    console.log("-----------------------------------\n");
+    appendFile("-----------------------------------\n");
 }
 
 function appendFile(arg) {
-  fs.appendFile(textFile, arg, function(err) {
-    if (err) {
-      console.log(err);
-    } else {}
-  });
+    fs.appendFile(textFile, arg, function (err) {
+        if (err) {
+            console.log(err);
+        } else { }
+    });
 }
